642cf175a0292d7a6f640c4d08534458e5a7a8e2
Askhat <jam.askhat@gmail.com>
2019-09-26 03:09:43 +0600

    the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let arr = expression.split("*");
+  let twoNum = 0;
+  let fiveNum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].slice(arr[i].length-2) == "!!") {
+      let count = +arr[i].slice(0, arr[i].length-2);
+      if (count%2 == 1) {
+        for (let j = 1; j <= count; j+=2) {
+          if (j%5 == 0) {
+            fiveNum++;
+          }
+          if (j%25 ==0) {
+            fiveNum++;
+          }
+        }
+      } else {
+        for (let j = 2; j <= count; j+=2) {
+          if (j%5 == 0) {
+            fiveNum++;
+          } else if (j%2 == 0) {
+            twoNum++;
+          } 
+          if (j%25 == 0) {
+            fiveNum++;
+          }
+        }
+      }
+    } else {
+      let count = +arr[i].slice(0, arr[i].length-1);
+      for (let j = 1; j <= count; j++) {
+        if (j%5 == 0) {
+          fiveNum++;
+        } else if (j%2 == 0) {
+          twoNum++;
+        } 
+        if (j%25 == 0) {
+          fiveNum++;
+        }
+      }
+    }
+  }
+  if (fiveNum < twoNum) {
+    return fiveNum;
+  } else {
+    return twoNum;
+  }
 }
